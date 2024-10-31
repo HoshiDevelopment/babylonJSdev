85f14b7924cd97283ee2654eddc455bfa02d357e
HoshiDev <berrycharlie055@gmail.com>
2024-10-31 16:25:21 +0000

    Added some scenes
    Shapes and colours

--- babylonProj/lighting01/src/createStartScene.ts
+++ babylonProj/lighting01/src/createStartScene.ts
@@ -22,8 +22,11 @@ import {
 
   
   function createLight(scene: Scene) {
-    const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
-    light.intensity = 0.7;
+    const light = new HemisphericLight("light", new Vector3(1, 1, 0), scene);
+    light.intensity = 2;
+    light.diffuse = new Color3(0, 0, 1);
+    light.specular = new Color3(0, 0, 1);
+    light.groundColor = new Color3(1, 0, 0);
     return light;
   }
   
